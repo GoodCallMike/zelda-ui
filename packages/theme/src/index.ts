a84c47be43638ccb6aaee7f93734a257299b6717
e189860 <michael.saylor@wnco.com>
2025-07-31 16:03:58 -0400

    Import utilities CSS in theme package index

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,3 +1,4 @@
-import './theme.css';
-import './tokens.css';
+export * from './tokens.css';
+export * from './theme.css';
+export * from './utilities/index.css';
 export { cn } from './utils';
--- packages/theme/src/utilities.css.ts
+++
@@ -1,5 +0,0 @@
-// Import CSS variables first
-import './variables.css';
-
-// Import organized utility modules
-import './utilities/index.css';
